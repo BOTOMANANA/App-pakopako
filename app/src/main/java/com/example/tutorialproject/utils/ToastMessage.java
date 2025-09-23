f99479993d69a8dfd591244595f3e33cad83d524
BOTOMANANA <antoniobotomanana@gmail.com>
2025-09-23 14:06:00 +0300

    code change

--- app/src/main/java/com/example/tutorialproject/utils/ToastMessage.java
+++ app/src/main/java/com/example/tutorialproject/utils/ToastMessage.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToastMessage {
-	private Context context;
-	private String message;
-
 	public static void showToast(Context context, String message) {
 		Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
 		toast.show();
