acfcbb3fdb9af51588a7bd643ae76595d6c053af
BOTOMANANA <antoniobotomanana@gmail.com>
2025-09-23 14:01:29 +0300

    code change

--- app/src/main/java/com/example/tutorialproject/controllers/AddNewCommandPage.java
+++ app/src/main/java/com/example/tutorialproject/controllers/AddNewCommandPage.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
